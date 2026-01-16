c20246b4a4a0cbe85eb2e44fcd189252a1402b91
noe <46964480+sallenoe@users.noreply.github.com>
2026-01-16 10:01:26 +0100

    Update Livre.h

--- src/Livre.h
+++ src/Livre.h
@@ -5,7 +5,16 @@
 
 class Livre
 {
+private: 
+    
 };
+enum genre {Littérature, Roman noir, Roman policier, Roman
+animalier, Roman d'amour, Roman de mœurs, Roman gothique, Roman courtois, Roman
+épistolaire, Roman-feuilleton, Roman graphique, Roman historique, Roman-photo, Roman
+picaresque, Roman-mémoires, Roman populaire, Roman d'aventures, Roman d'anticipation,
+Roman d'espionnage, Roman d'apprentissage, Roman de chevalerie, Roman autobiographique,
+Nouveau roman, Roman chevaleresque, Conte, Nouvelle};
+
 
 #endif
 
